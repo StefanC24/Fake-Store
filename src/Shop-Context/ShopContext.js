9f67e979b1246d1342d718825e24051299a736a0
StefanC24 <stefancepeleaga@gmail.com>
2023-05-11 01:19:58 +0100

    fixed the remove from cart and favourites functionality

--- src/Shop-Context/ShopContext.js
+++ src/Shop-Context/ShopContext.js
@@ -1,56 +1,51 @@
-import { createContext, useState, useEffect } from "react";
-import React from "react";
+import { createContext, useState, useEffect } from "react"
+import React   from 'react'
 
-export const ShopContext = createContext();
+export const ShopContext = createContext()
 
 const ShopContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState([]);
-  const [favItems, setFavItems] = useState([]);
-
-  useEffect(() => {
-    setCartItems(JSON.parse(window.localStorage.getItem("cartItems")));
-    setFavItems(JSON.parse(window.localStorage.getItem("favItems")));
-  }, []);
-
-  const addItemToCart = (productToBeAdded) => {
-    let newItem;
-    newItem = window.localStorage.getItem("cartItems");
-    if (newItem) {
-      newItem = JSON.parse(newItem);
-      newItem = [...newItem, productToBeAdded];
-    } else {
-      newItem = [productToBeAdded];
+    
+    const [cartItems, setCartItems] = useState([])
+    const [favItems, setFavItems] = useState([])
+      
+    useEffect(() => {
+        setCartItems(JSON.parse(window.localStorage.getItem('cartItems')))
+        setFavItems(JSON.parse(window.localStorage.getItem('favItems')))
+    }, [])
+        
+    const addItemToCart = (productToBeAdded) =>{
+        let newItem;
+        newItem = window.localStorage.getItem('cartItems')
+        if(newItem){
+            newItem = JSON.parse(newItem)
+            newItem = [...newItem, productToBeAdded]
+        }
+        else{
+            newItem = [productToBeAdded]
+        }
+        window.localStorage.setItem('cartItems', JSON.stringify(newItem))
+        setCartItems(newItem);
     }
-    window.localStorage.setItem("cartItems", JSON.stringify(newItem));
-    setCartItems(newItem);
-  };
 
-<<<<<<< HEAD
     const removeItemFromCart = (productToBeRemovedId) =>{
         let newItem = JSON.parse(window.localStorage.getItem('cartItems'))
         newItem = newItem.filter(product => product.id !== productToBeRemovedId)
         window.localStorage.setItem('cartItems', JSON.stringify(newItem));
         setCartItems(newItem);
         }
-=======
-  //   remover the curly braces from the parameter
-  const removeItemFromCart = ({ productToBeRemovedId }) => {
-    let newItem = JSON.parse(window.localStorage.getItem("cartItems"));
-    newItem = newItem.filter((product) => product.id !== productToBeRemovedId);
-    window.localStorage.setItem("cartItems", JSON.stringify(newItem));
-    setCartItems(newItem);
-  };
->>>>>>> f41100daf4c17ae8f9be98c40e28375cce5c815e
 
-  const addItemToFavourites = (productToBeAdded) => {
-    let newItem = window.localStorage.getItem("favItems");
-    if (newItem) {
-      newItem = JSON.parse(newItem);
-      newItem = [...newItem, productToBeAdded];
-    } else {
-      newItem = [productToBeAdded];
+    const addItemToFavourites = (productToBeAdded) =>{
+        let newItem = window.localStorage.getItem('favItems')
+        if(newItem){
+            newItem = JSON.parse(newItem)
+            newItem = [...newItem, productToBeAdded]
+        }
+        else{
+            newItem = [productToBeAdded]
+        }
+        window.localStorage.setItem('favItems', JSON.stringify(newItem))
+        setFavItems(newItem);
     }
-<<<<<<< HEAD
     const removeItemFromFav = (productToBeRemovedId) =>{
         let newItem = JSON.parse(window.localStorage.getItem('favItems'))
         newItem = newItem.filter(product => product.id !== productToBeRemovedId)
@@ -59,38 +54,16 @@ const ShopContextProvider = (props) => {
 
     }
     
-=======
-    window.localStorage.setItem("favItems", JSON.stringify(newItem));
-    setFavItems(newItem);
-  };
->>>>>>> f41100daf4c17ae8f9be98c40e28375cce5c815e
 
-  //   remover the curly braces from the parameter
-  const removeItemFromFav = ({ productToBeRemovedId }) => {
-    let newItem = JSON.parse(window.localStorage.getItem("favItems"));
-    newItem = newItem.filter((product) => product.id !== productToBeRemovedId);
-    console.log(newItem);
-    window.localStorage.setItem("favItems", JSON.stringify(newItem));
-    setFavItems(newItem);
-    // You are missing the state update here like in the add to favorites
-  };
+    const contextValue = {
+        cartItems, favItems, addItemToCart, removeItemFromCart, removeItemFromFav, addItemToFavourites, setCartItems, setFavItems
+    }
 
-  const contextValue = {
-    cartItems,
-    favItems,
-    addItemToCart,
-    removeItemFromCart,
-    removeItemFromFav,
-    addItemToFavourites,
-    setCartItems,
-    setFavItems,
-  };
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+  )
+}
 
-  return (
-    <ShopContext.Provider value={contextValue}>
-      {props.children}
-    </ShopContext.Provider>
-  );
-};
-
-export default ShopContextProvider;
+export default ShopContextProvider
